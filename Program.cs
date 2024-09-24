a0aa8c6a30c9320a7d198075bb15adbd77f233cd
ProfAmquintal <181018606+ProfAmquintal@users.noreply.github.com>
2024-09-24 20:15:33 +0100

    Foi acrescentado código para testar

--- Program.cs
+++ Program.cs
@@ -11,9 +11,12 @@ namespace Calculos
         static void Main(string[] args)
         {
             Console.WriteLine("Aplicação Cálculos");
-            Console.WriteLine("4+2=", aritmetica.Somar(4, 2));
+            Console.WriteLine($"4+2=", aritmetica.Somar(4, 2));
             Console.WriteLine("4-2=", aritmetica.Subtrair(4, 2));
-
+            double t1 = Conversao.ConverterTemperatura(Conversao.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversao.ConverterTemperatura(Conversao.ConversaoTemperatura.FahrenheitCelsius, 100);
+            Console.WriteLine("36 graus Celsius=",t1, "Fahrenheit");
+            Console.WriteLine("100 graus Fahrenheit=", t2, "Celsius");
             Console.ReadKey();
         }
     }
